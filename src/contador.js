17b0bb40169f95cff69b6c10652678d70f999c73
Carmen Quispe <carmen.quispe@ucb.edu.bo>
2023-09-05 10:33:01 -0400

    contar palabra

---
+++ src/contador.js
@@ -0,0 +1,31 @@
+
+
+function es_igual(palabra) {
+    contador = 0
+    if (palabra === palabra)
+        contador = contador +1
+
+    return contardor;
+  }
+  
+ // export default contador;
+
+
+function contarPalabra(frase){
+    length = frase;//cantidad de palabras
+    vecFrase[0]=frase//meter la frase en un vector
+
+    for(i=0;i<length;i++){
+
+        vecFrase[i]=frase
+
+    }
+
+    for(i=0;i<length;i++){
+
+        es_igual(i);//verificar palabras repetidas y contarlas en otro vector
+
+    }
+
+}
+export default contarPalabra;
