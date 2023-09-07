b631da987e1814682884d3ec8bb0b3a2446082a1
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-09-07 09:55:25 -0400

    cuenta 2 palabras

--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,17 @@
-function sumar(a, b) {
-  return a + b;
+function contarRepeticion(cadena) {
+  let resultado = {};
+  if (cadena.length > 0) {
+    let listaPalabras = cadena.split(" ");
+    for (let i = 0; i < listaPalabras.length; i++) {
+      if (resultado[listaPalabras[i]]) {
+        resultado[listaPalabras[i]] += 1;
+      } else {
+        resultado[listaPalabras[i]] = 1;
+      }
+    }
+  }
+  return resultado;
 }
 
-export default sumar;
+export default contarRepeticion;
+
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,7 +1,21 @@
 import contarPalabra from "./contador.js";
+import contarRepeticion from "./sumador.js";
 
-describe("Contar primer repetido", () => {
+describe("Contar  repetido", () => {
   it("deberia contar el mas repetido", () => {
-    expect(contarPalabra("password PASWORD")).toEqual("pasword: 2");
+    expect(contarPalabra("password password")).toEqual("password: 2");
+  });
+});
+describe("Cadena Vacia", () => {
+  it("muestra cadena vacia ", () => {
+    expect(contarRepeticion(" ")).toEqual({});
+  });
+
+  it("deberia mostrar una palabra", () => {
+    expect(contarRepeticion(" password ")).toEqual({password: 1});
+  });
+  
+  it("deberia contar 2 palabras mas repetido", () => {
+    expect(contarRepeticion(" password pasword ")).toEqual({password: 2});
   });
 });
