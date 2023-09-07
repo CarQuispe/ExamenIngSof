e56407d3f21dce630c55e4659f953e9978df2286
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-09-07 09:27:26 -0400

    Una palabra

--- src/contador.js
+++ src/contador.js
@@ -1,31 +1,40 @@
+function es_igual(palabra, vecFrase) {
+    let contador = 0;
 
-
-function es_igual(palabra) {
-    contador = 0
-    if (palabra === palabra)
-        contador = contador +1
-
-    return contardor;
-  }
-  
- // export default contador;
-
-
-function contarPalabra(frase){
-    length = frase;//cantidad de palabras
-    vecFrase[0]=frase//meter la frase en un vector
-
-    for(i=0;i<length;i++){
-
-        vecFrase[i]=frase
-
+    for (let i = 0; i < vecFrase.length; i++) {
+        if (palabra.toLowerCase() === vecFrase[i].toLowerCase()) { // Comparación sin distinción entre mayúsculas y minúsculas
+            contador++;
+        }
     }
 
-    for(i=0;i<length;i++){
-
-        es_igual(i);//verificar palabras repetidas y contarlas en otro vector
-
-    }
-
+    return contador;
 }
+
+function contarPalabra(frase) {
+    const palabras = frase.split(' '); // Divide la frase en palabras usando espacios como separadores
+    const vecFrase = palabras.slice(); // Crea una copia de las palabras en un vector
+
+    for (let i = 0; i < palabras.length; i++) {
+        vecFrase[i] = palabras[i];
+    }
+
+    const contadorPalabrasRepetidas = {};
+
+    for (let i = 0; i < palabras.length; i++) {
+        const palabra = palabras[i].toLowerCase(); // Convertir a minúsculas para la comparación sin distinción entre mayúsculas y minúsculas
+        const contador = es_igual(palabra, vecFrase);
+        contadorPalabrasRepetidas[palabra] = contador;
+    }
+
+    // Crear una cadena de resultado
+    let resultado = '';
+    for (const palabra in contadorPalabrasRepetidas) {
+        resultado += `${palabra}: ${contadorPalabrasRepetidas[palabra]} `;
+    }
+
+    return resultado.trim();
+}
+
 export default contarPalabra;
+
+
